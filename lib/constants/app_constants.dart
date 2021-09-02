ada1af6ff9b057249ab0c75d59017cb8d94cdee3
lester0311 <lester.dinopol@lsu.edu.ph>
2021-09-02 11:22:36 +0800

    BMI

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color mainHexColor = HexColor("#421E22");
+final Color accentHexColor = HexColor("#EFA355");
